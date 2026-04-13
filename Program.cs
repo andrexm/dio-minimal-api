aacb22d9f208393f6ecc3d9f9ec1d8c4d8b93522
André Nascimento <andre.neospace@gmail.com>
2026-04-13 13:17:50 -0300

    feat: login dto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using MinimalAPI.DTOs;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -16,9 +18,3 @@ app.MapPost("/login", (LoginDTO loginDTO) =>
 });
 
 app.Run();
-
-public class LoginDTO
-{
-    public string Email { get; set; } = default!;
-    public string Password { get; set; } = default!;
-}
