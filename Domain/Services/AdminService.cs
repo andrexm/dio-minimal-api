634816a59f31c5e0d7fcf9b45702c46a1819fc60
André Nascimento <andre.neospace@gmail.com>
2026-04-13 20:45:42 -0300

    feat: admin service

---
+++ Domain/Services/AdminService.cs
@@ -0,0 +1,23 @@
+using MinimalAPI.Domain.Entities;
+using MinimalAPI.Domain.Interfaces;
+using MinimalAPI.DTOs;
+using MinimalAPI.Infra.DB;
+
+namespace MinimalAPI.Domain.Services;
+
+public class AdminService : IAdminService
+{
+    private readonly DBContext _db;
+
+    public AdminService(DBContext db)
+    {
+        _db = db;
+    }
+
+    public Admin? Login(LoginDTO loginDTO)
+    {
+        var admin = _db.Admins.FirstOrDefault(a => a.Email == loginDTO.Email && a.Password == loginDTO.Password);
+        return admin ?? null;
+    }
+
+}
