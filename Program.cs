595b7eac6aace421d0910a486e63dda67a666ec6
André Nascimento <andre.neospace@gmail.com>
2026-04-13 16:47:32 -0300

    feat: database context

--- Program.cs
+++ Program.cs
@@ -1,6 +1,18 @@
+using Microsoft.EntityFrameworkCore;
 using MinimalAPI.DTOs;
+using MinimalAPI.Infra.DB;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DBContext>(options =>
+{
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+    );
+});
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
