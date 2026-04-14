2e111257e8e182c82459b6bc6d6697bc1fd313f2
André Nascimento <andre.neospace@gmail.com>
2026-04-14 17:35:54 -0300

    feat: vehicles

---
+++ Domain/DTOs/VehicleDTO.cs
@@ -0,0 +1,9 @@
+namespace MinimalAPI.Domain.DTOs;
+
+public record VehicleDTO
+{
+    public string Name { get; set; } = default!;
+    public string Brand { get; set; } = default!;
+    public int Year { get; set; } = default!;
+    public string Model { get; set; } = default!;
+}
