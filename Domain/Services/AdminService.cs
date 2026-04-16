4ac1a2ee995bba05777e58dd5f71fd579ce59e88
André Nascimento <andre.neospace@gmail.com>
2026-04-16 12:13:58 -0300

    feat: admin service

--- Domain/Services/AdminService.cs
+++ Domain/Services/AdminService.cs
@@ -20,4 +20,26 @@ public class AdminService : IAdminService
         return admin ?? null;
     }
 
+    public Admin CreateAdmin(Admin admin)
+    {
+        _db.Admins.Add(admin);
+        _db.SaveChanges();
+        return admin;
+    }
+
+    public Admin? GetAdminById(int id)
+    {
+        var admin = _db.Admins.FirstOrDefault(a => a.Id == id);
+        return admin ?? null;
+    }
+
+    public List<Admin> GetAllAdmins(int? pageNumber = null)
+    {
+        var query = _db.Admins.AsQueryable();
+        if (pageNumber.HasValue)
+        {
+            query = query.Skip((pageNumber.Value - 1) * 10).Take(10);
+        }
+        return query.ToList();
+    }
 }
