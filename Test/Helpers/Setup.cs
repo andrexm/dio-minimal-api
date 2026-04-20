cfe166aece3a1e3a3a23f75a17933153c81073ac
André Nascimento <andre.neospace@gmail.com>
2026-04-20 18:57:25 -0300

    feat: set up application

---
+++ Test/Helpers/Setup.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using MinimalAPI;
+using MinimalAPI.Domain.Interfaces;
+using Test.Mocks;
+
+namespace Test.Helpers;
+
+public class Setup
+{
+    public const string PORT = "5000";
+    public static TestContext testContext { get; set; } = default!;
+    public static WebApplicationFactory<Startup> http { get; set; } = default!;
+    public static HttpClient client { get; set; } = default!;
+
+    public static void ClassInit(TestContext testContext)
+    {
+        Setup.testContext = testContext;
+        Setup.http = new WebApplicationFactory<Startup>();
+
+        Setup.http = Setup.http.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddScoped<IAdminService, AdminServiceMock>();
+            });
+        });
+
+        Setup.client = Setup.http.CreateClient();
+    }
+
+    public static void ClassCleanup()
+    {
+        Setup.http.Dispose();
+    }
+}
