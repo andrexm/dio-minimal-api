707fb1ae6be1912d63c16ecdbc4ab34fe35588ff
André Nascimento <andre.neospace@gmail.com>
2026-04-20 18:57:35 -0300

    feat: admin service mock

---
+++ Test/Mocks/AdminServiceMock.cs
@@ -0,0 +1,32 @@
+using MinimalAPI.Domain.DTOs;
+using MinimalAPI.Domain.Entities;
+using MinimalAPI.Domain.Interfaces;
+
+namespace Test.Mocks;
+
+public class AdminServiceMock : IAdminService
+{
+    private static List<Admin> admins = new List<Admin>();
+
+    public Admin? Login(LoginDTO loginDTO)
+    {
+        return admins.FirstOrDefault(a => a.Email == loginDTO.Email && a.Password == loginDTO.Password) ?? null!;
+    }
+
+    public Admin GetAdminById(int id)
+    {
+        return admins.FirstOrDefault(a => a.Id == id) ?? null!;
+    }
+
+    public Admin CreateAdmin(Admin admin)
+    {
+        admin.Id = admins.Count + 1;
+        admins.Add(admin);
+        return admin;
+    }
+
+    public List<Admin> GetAllAdmins(int? pageNumber)
+    {
+        return admins;
+    }
+}
